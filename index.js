a9b98f0081b1e715b3ed6f9dd3c06981d4456763
John Westwig <john.westwig@nytimes.com>
2022-07-18 17:29:22 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGJSc82VsKbRqiTBi55ZfMsqBfHvguFNE",
+  authDomain: "liarsdice-64f75.firebaseapp.com",
+  projectId: "liarsdice-64f75",
+  storageBucket: "liarsdice-64f75.appspot.com",
+  messagingSenderId: "487174055686",
+  appId: "1:487174055686:web:d7bab09d813c4bd3566087"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
